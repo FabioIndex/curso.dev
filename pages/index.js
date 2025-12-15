e417802d2b0f0e54c107a68dc5cab02fe4d45f76
Fabio <134175915+FabioIndex@users.noreply.github.com>
2025-12-15 23:34:11 +0000

    add prettierignor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Andreia eu amo vc. Se voce me ama, de uma risadinha!😀</h1>
+function Home() {
+  return <h1>Andreia eu amo vc. Se voce me ama, de uma risadinha!😀</h1>;
 }
 
 export default Home;
