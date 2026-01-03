01e8b3cc3e633fdf66402802d0d7bcc965473793
Fabio <134175915+FabioIndex@users.noreply.github.com>
2026-01-03 18:42:24 +0000

    "add sql injection correçao"

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,10 +7,11 @@ async function status(request, response) {
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
   const maxConnections = await database.query(" SHOW max_connections;");
   const maxConnectionsValue = parseInt(maxConnections.rows[0].max_connections);
-  const statusdatabase = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity   WHERE datname ='postgres';",
-  );
-  console.log(statusdatabase.rows[0]);
+  const statusdatabaseName = process.env.POSTGRES_DATABASE;
+  const statusdatabase = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname =$1;",
+    values: [statusdatabaseName],
+  });
 
   const statusdatabaseValue = parseInt(statusdatabase.rows[0].count);
 
